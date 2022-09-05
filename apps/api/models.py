afad1fa00bc8a9eed4feaeec728476429f1988e9
jordan95v <jordan.dfrsne@gmail.com>
2022-09-05 13:35:13 +0200

    Added model.

--- apps/api/models.py
+++ apps/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Standard(models.Model):
+    numdos: models.CharField = models.CharField(max_length=8, unique=True)
+    numdos_vl: models.CharField = models.CharField(max_length=8, null=True, blank=True)
+    ancart: models.CharField = models.CharField(max_length=25, null=True, blank=True)
+    channel: models.CharField = models.CharField(max_length=3, null=True, blank=True)
+    stage: models.FloatField = models.FloatField(null=True, blank=True)
+    ve: models.CharField = models.CharField(max_length=3, null=True, blank=True)
+    format: models.CharField = models.CharField(max_length=5, null=True, blank=True)
